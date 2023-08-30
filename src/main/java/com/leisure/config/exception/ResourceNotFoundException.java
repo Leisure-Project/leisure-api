f2bed38f4da1ff891c856a4f2d209548dd67582a
AntonioFerrandiz <antoniojfb01@gmail.com>
2023-08-30 00:47:22 -0500

    feat: add new error message

--- src/main/java/com/leisure/config/exception/ResourceNotFoundException.java
+++ src/main/java/com/leisure/config/exception/ResourceNotFoundException.java
@@ -8,6 +8,9 @@ public class ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException(String entity, Long id) {
         super(String.format("%s con id %d no existe.", entity, id));
     }
+    public ResourceNotFoundException(String entity, String dni) {
+        super(String.format("%s con dni %s no existe.", entity, dni));
+    }
     public ResourceNotFoundException(String message){
         super(message);
     }
