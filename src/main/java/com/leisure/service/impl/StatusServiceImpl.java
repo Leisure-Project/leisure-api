078d74e5eb268c435c62d76884653c0486a31276
AntonioFerrandiz <antoniojfb01@gmail.com>
2024-02-28 17:27:33 -0500

    fix status seed

--- src/main/java/com/leisure/service/impl/StatusServiceImpl.java
+++ src/main/java/com/leisure/service/impl/StatusServiceImpl.java
@@ -1,9 +1,7 @@
 package com.leisure.service.impl;
 
 import com.leisure.config.exception.ResourceNotFoundException;
-import com.leisure.entity.Role;
 import com.leisure.entity.Status;
-import com.leisure.entity.enumeration.Rolname;
 import com.leisure.entity.enumeration.StatusName;
 import com.leisure.repository.StatusRepository;
 import com.leisure.service.StatusService;
@@ -15,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
@@ -22,6 +21,10 @@ public class StatusServiceImpl implements StatusService {
     @Autowired
     private StatusRepository statusRepository;
     private static String[] DEFAULT_STATUS = { "ACTIVO","INACTIVO"};
+    private static String[] DEFAULT_STATUS_DESCRIPTION = {
+            "El usuario ha realizado su consumo mínimo mensual.",
+            "El usuario NO ha realizado su consumo mínimo mensual."
+    };
     @Override
     public Status createStatus(Status status) {
         return statusRepository.save(status);
@@ -43,11 +46,13 @@ public class StatusServiceImpl implements StatusService {
 
     @Override
     public void seed() {
+        AtomicInteger i = new AtomicInteger();
         Arrays.stream(DEFAULT_STATUS).forEach(name -> {
             StatusName statusName = StatusName.valueOf(name);
             if(!statusRepository.existsByName(statusName)) {
-                statusRepository.save((new Status()).withName(statusName));
+                statusRepository.save((new Status()).withName(statusName).withDescription(DEFAULT_STATUS_DESCRIPTION[i.get()]));
             }
+            i.getAndIncrement();
         } );
     }
 
